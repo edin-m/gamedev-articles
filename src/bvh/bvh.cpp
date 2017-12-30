147dc3b6a442431a75234039ee7776ef1f05719a
Edin Mujagic <edinmujagic2@gmail.com>
2017-12-30 23:48:57 +0100

    Add assimp library

---
+++ src/bvh/bvh.cpp
@@ -0,0 +1,11 @@
+#include "bvh.h"
+
+#include <iostream>
+
+namespace bvh {
+
+Bvh::Bvh() {
+  std::cout << "output" << std::endl;
+}
+
+}
---
+++ src/bvh/bvh.h
@@ -0,0 +1,13 @@
+#ifndef BVH_H
+#define BVH_H
+
+namespace bvh {
+
+class Bvh {
+public:
+  Bvh();
+};
+
+}
+
+#endif
