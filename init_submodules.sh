26ccd6d6472bbd3438a3321a1fd573a35aedc3e7
Edin M <edinmujagic2 at gmail>
2018-07-09 22:51:49 +0200

    Improve submodules

--- init_submodules.sh
+++ init_submodules.sh
@@ -1,31 +1,27 @@
-git submodule update --init
-cd lib/glew-2.1.0
-git checkout 3a8eff77da3658c13fbd3634c943d5251d76322c
-cd ../..
-
-git submodule update --init
-cd lib/glm
-git checkout 6fa203eeb7fbcbb6f620501fad40359c8a456049
-cd ../..
-
-cd lib/glfw
-git checkout 999f3556fdd80983b10051746264489f2cb1ef16
-cd ../..
+git submodule update --init --recursive
 
 cd lib/DevIL
 git checkout e34284a7e07763769f671a74b4fec718174ad862
 cd ../..
 
-cd lib/soil
-git checkout 4fff135429c4c7996791c019e6cc20a1e9fde568
+cd lib/alure
+git checkout 3a229dfb5429220b2773015a79aae0361e62be48
+cd ../..
+
+cd lib/argh
+git checkout v1.2.1
 cd ../..
 
 cd lib/assimp
-git checkout 80799bdbf90ce626475635815ee18537718a05b1
+git checkout v4.1.0
 cd ../..
 
-cd lib/openexr
-git checkout v2.2.1
+cd lib/easyloggingpp
+git checkout v9.96.4
+cd ../..
+
+cd lib/eigen
+git checkout 3.3.4
 cd ../..
 
 cd lib/freeglut
@@ -36,30 +32,39 @@ cd lib/freetype-gl
 git checkout 507cc68511465981d32257268fee184b79cc69f7
 cd ../..
 
-cd lib/assimp
-git checkout v4.1.0
+cd lib/glew
+git checkout glew-2.1.0
 cd ../..
 
-cd lib/argh
-git checkout v1.2.0
+cd lib/glfm
+git checkout 0.9.0
 cd ../..
 
-cd lib/zlib
-git checkout v1.2.11
+cd lib/glfw
+git checkout 3.2.1
 cd ../..
 
-cd lib/alure
-git checkout 82436dd3eb834635140a9e8d3224376ae2d6fbf5
-cd ../..
-
-cd lib/vorbis
-git checkout v1.3.5
+cd lib/glm
+git checkout 0.9.9.0
 cd ../..
 
 cd lib/ogg
 git checkout v1.3.3
 cd ../..
 
-cd lib/eigen
-git checkout 3.3.4
+cd lib/openexr
+git checkout v2.2.1
 cd ../..
+
+cd lib/soil
+git checkout 4fff135429c4c7996791c019e6cc20a1e9fde568
+cd ../..
+
+cd lib/vorbis
+git checkout v1.3.6
+cd ../..
+
+cd lib/zlib
+git checkout v1.2.11
+cd ../..
+
